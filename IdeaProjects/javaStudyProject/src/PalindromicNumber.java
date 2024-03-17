2b02ea888d5e95a96ff8b05e453f535919284a84
ZhongYuheng0218 <163721340+ZhongYuheng0218@users.noreply.github.com>
2024-03-17 17:25:35 +0800

    Update PalindromicNumber.java

--- IdeaProjects/javaStudyProject/src/PalindromicNumber.java
+++ IdeaProjects/javaStudyProject/src/PalindromicNumber.java
@@ -5,8 +5,9 @@ public class CirculationTest2 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         //用户输入一个数字
-        //int num1 = sc.nextInt();
-        int num1 = 0;
+        System.out.println("Please enter a positive integer:");
+        int num1 = sc.nextInt();
+        //int num1 = 0;
 
         //将num1拆解，并逆序成为一个新的数num2
         int num2 = 0;
@@ -16,13 +17,13 @@ public class CirculationTest2 {
 
         while(temp > 0){
             //将个位数用remainder保存
-            int remainder = 0;
-            remainder = temp % 10;
-            temp/=10;
-            num2 = num2*10 + remainder;
+            int remainder = 0;    //初始化remainder为0
+            remainder = temp % 10;//将temp的个位数字赋值给remainder
+            temp/=10;    //去除temp的个位数
+            num2 = num2*10 + remainder;    //将逆序数存储到变量num2中
         }
 
-        System.out.println(num1 == num2 ? num1 + "是回文数" : num1 + "不是回文数");
+        System.out.println(num1 == num2 ? num1 + "Is A Palindromic Number" : num1 + "Is Not A Palindromic Number");
 
     }
 }
